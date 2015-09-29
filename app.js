78651e3baa09bb4177ac5e83b2b5c4c605ec9e8f
Amasa <alexleone89@gmail.com>
2015-09-29 15:06:37 -0500

    First stab at a layout for the site, some rough content

---
+++ app.js
@@ -0,0 +1,69 @@
+/*global angular */
+(function (app) {
+    "use strict";
+
+    app.controller("TabController", ["$rootScope", function ($rootScope) {
+        var currentTab;
+
+        $rootScope.select = function (tab) {
+            currentTab = tab;
+        };
+
+        $rootScope.selected = function (tab) {
+            return tab === currentTab;
+        };
+
+        $rootScope.select("projects");
+    }]);
+
+    app.controller("ProjectController", function () {
+        var controller = this,
+            projects = [
+                {
+                    name: "Ptolemy",
+                    languages: ["Java", "C#"],
+                    yearStart: 2009,
+                    yearEnd: 2014
+                },
+                {
+                    name: "The Farmer Game",
+                    languages: ["Basic/Other"],
+                    yearStart: 2003,
+                    yearEnd: 2008
+                },
+                {
+                    name: "Color Clock",
+                    languages: ["Javascript", "CSS", "HTML"],
+                    yearStart: 2015,
+                    yearEnd: 2015
+                },
+                {
+                    name: "Costume Selector",
+                    languages: ["C#", "JavaScript", "Angular", "CSS", "HTML", "ASP.NET MVC"],
+                    yearStart: 2015,
+                    yearEnd: 2015
+                },
+                {
+                    name: "Game Table",
+                    languages: ["C#", "JavaScript", "CSS", "HTML", "ASP.NET MVC", "Angular", "jQuery"],
+                    yearStart: 2015,
+                    yearEnd: null
+                }
+            ];
+
+        controller.list = projects;
+
+        controller.getYearText = function (project) {
+            if (!project.yearEnd && project.yearStart) {
+                return project.yearStart + " - present";
+            }
+
+            if (project.yearStart === project.yearEnd) {
+                return project.yearEnd;
+            }
+
+            return project.yearStart + " - " + project.yearEnd;
+        };
+    });
+
+}(angular.module("alexleone", [])));
